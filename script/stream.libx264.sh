3a72da5f9d881bc5552bb83d782a3670d9a2cae1
ngochieu642 <ngochieu10041997@gmail.com>
2020-03-25 14:26:35 +0700

    Change name show->hls

--- script/stream.libx264.sh
+++ script/stream.libx264.sh
@@ -1,1 +1,1 @@
-ffmpeg -rtsp_transport tcp -i rtsp://admin:1987@Thanh@192.168.1.37:5555/profile2/media.smp -framerate 30 -video_size 1280x720 -vcodec libx264 -preset veryfast -f flv rtmp://localhost/show/stream
+ffmpeg -rtsp_transport tcp -i rtsp://admin:1987@Thanh@192.168.1.37:5555/profile2/media.smp -framerate 30 -video_size 1280x720 -vcodec libx264 -preset veryfast -f flv rtmp://localhost/hls/stream
